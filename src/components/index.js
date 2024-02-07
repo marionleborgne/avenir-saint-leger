c4055a9a8b6534abe2f5d96c850e0f7c3c5a6d72
Marion Le Borgne <leborgne.mar@gmail.com>
2024-02-06 23:51:26 -0800

    Program

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export { default as Hero } from './Hero';
 export { default as Logo } from './Logo';
 export { default as Background } from './Background';
 export { Avatars } from './Avatars';
+export { default as Program } from './Program';
