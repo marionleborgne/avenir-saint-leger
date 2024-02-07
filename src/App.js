36c8da3f78b9c48971344d5b3ec4c6edd3b7750d
Marion Le Borgne <leborgne.mar@gmail.com>
2024-02-06 22:41:00 -0800

    Detect mobile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { ThemeProvider } from '@mui/material/styles';
+import { Typography } from '@mui/material';
+import { isMobile } from 'react-device-detect';
 
 import { Background, TopBar, Avatars, Hero } from './components';
 import './App.css';
@@ -12,6 +14,7 @@ function App() {
         <TopBar />
         <Hero />
         <Background />
+        <Typography variant={isMobile ? "h4" : "h2"} sx={{ margin: 10 }}>Une équipe à votre service</Typography>
         <Avatars />
       </div>
     </ThemeProvider>
