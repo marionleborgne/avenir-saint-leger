ec8ad2a6bf3ed2b18acc5820dfa2b4dfaa18a646
Marion Le Borgne <leborgne.mar@gmail.com>
2024-02-06 22:53:53 -0800

    fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const { container } = render(<App />);
+  const linkElement = container.firstChild;
   expect(linkElement).toBeInTheDocument();
 });
