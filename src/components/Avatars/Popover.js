5a3a8ac8255dff98c5dbbb3b25893d120755c1b6
Todd Medema <toddmedema@gmail.com>
2024-02-08 12:45:00 -0800

    fix error

--- src/components/Avatars/Popover.js
+++ src/components/Avatars/Popover.js
@@ -44,7 +44,7 @@ export default function MouseOverPopover({ name, description }) {
           vertical: 'bottom',
           horizontal: 'left',
         }}
-        disableScrollLock='true'
+        disableScrollLock={true}
         transformOrigin={{
           vertical: 'top',
           horizontal: 'left',
