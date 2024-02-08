9b12bb0ccc8d5726283328bc6fc092ccc277a858
Marion Le Borgne <leborgne.mar@gmail.com>
2024-02-07 22:34:45 -0800

    data entry update

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -1,14 +1,20 @@
 export default [
   {
-    title: 'Project 1',
-    description: '',
-    shortTerm: 'foo',
-    longTerm: 'bar' 
+    title: 'Participation citoyenne',
+    shortTerm: <div>
+      <p>{`Réunions régulières des commissions mixtes élus-citoyens relatives :`}</p>
+      <ul>
+        <li>{`à l'urbanisme, aux finances,  aux affaires scolaires, aux affaires sociales, à l'environnement,`}</li>
+        <li>{`Commission des fêtes (forum des associations plus festif)`}</li>
+        <li>{`Commission jeunesse (proposer un salon des métiers, et forum d'orientation, réunion  avec un expert PARCOURS'SUP, accompagnement)`}</li>
+        <li>{`Consultations  citoyennes pour des projets précis (évolution du centre aéré, cantine, micro-crèche, réflexion sur la sécurité du village (vigilance citoyenne…), création d'un espace multisports)`}</li>
+      </ul>
+    </div>,
+    longTerm: ''
   },
   {
     title: 'Project 2',
-    description: '',
     shortTerm: 'foo',
-    longTerm: 'bar' 
+    longTerm: 'bar'
   }
 ];
