20cb910b8f5a9839a432a562e52836e04a5f5bf7
Marion Le Borgne <leborgne.mar@gmail.com>
2024-02-08 22:31:08 -0800

    no more max char limit

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,10 +47,6 @@ module.exports = {
       'error',
       'always'
     ],
-    'max-len': [
-      2,
-      { 'code': 90 }
-    ],
     'react/react-in-jsx-scope': 'off',
     'react/jsx-uses-react': 'off',
     'react/display-name': 'off',
