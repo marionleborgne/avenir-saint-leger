fd71c2409e8ec549ca553ce92ee80af917ce61d2
Marion Le Borgne <leborgne.mar@gmail.com>
2024-02-08 21:23:01 -0800

    small cleanup

--- src/components/Projects/data.js
+++ src/components/Projects/data.js
@@ -39,7 +39,7 @@ export default [
   },
   {
     title: 'Diffusion de linformation',
-    shortTerm: (<div>
+    shortTerm: (
       <List>
         <Item
           text={`Panneaux d'affichage numérique`}
@@ -51,7 +51,7 @@ export default [
           text={`Valorisation des commerces grâce à une vitrine numérique`}
         />
       </List>
-    </div>),
+    ),
     longTerm: ''
   },
   {
@@ -85,7 +85,7 @@ export default [
         />
       </List>
     </div>),
-    longTerm: (<div>
+    longTerm: (
       <List>
         <Item
           text={`Réflexion sur l'agrandissement de la cantine et la création d'une cuisine dans la cantine de l'école pour la transformation des produits sur place`}
@@ -94,11 +94,11 @@ export default [
           text={`Réflexion sur la création d' un bâtiment dédié au centre de loisirs`}
         />
       </List>
-    </div>)
+    )
   },
   {
     title: 'Sécurisation de la circulation',
-    shortTerm:  (<div>
+    shortTerm: (
       <List>
         <Item
           text={`Déplacer certains passages piétons vers des endroits plus sûrs`}
@@ -110,18 +110,18 @@ export default [
           text={`Matérialiser les zones de vitesse 30km/h`}
         />
       </List>
-    </div>),
-    longTerm: (<div>
+    ),
+    longTerm: (
       <List>
         <Item
           text={`Réflexions sur l'aménagement des trottoirs et des accès aux établissements publics et commerces pour les personnes à mobilité réduite`}
         />
       </List>
-    </div>)
+    )
   },
   {
     title: 'Aménagement des espaces publics et sportifs',
-    shortTerm:  (<div>
+    shortTerm: (
       <List>
         <Item
           text={`Mettre à disposition des poubelles et des toutounettes`}
@@ -145,8 +145,8 @@ export default [
           text={`Installation du « workout (espace culture physique de plein air) » demandé par la commission jeunes en 2020`}
         />
       </List>
-    </div>),
-    longTerm: (<div>
+    ),
+    longTerm: (
       <List>
         <Item
           text={`Mise aux normes sanitaires de la cuisine de la maison du village`}
@@ -161,18 +161,18 @@ export default [
           text={`Consultation en vue de la création d'un complexe multisports vers le terrain de foot`}
         />
       </List>
-    </div>)
+    )
   },
   {
     title: 'Création dun espace de coworking',
     shortTerm: '',
-    longTerm: (<div>
+    longTerm: (
       <List>
         <Item
           text={`Consultation dans le but de créer un espace de coworking avec un lieu de convivialité intergénérationnel (café, salon …)`}
         />
       </List>
-    </div>)
+    )
   },
   // Nos solidarités
   {
@@ -182,7 +182,7 @@ export default [
   },
   {
     title: `Développer le Centre Communal d'Action Sociale`,
-    shortTerm: (<div>
+    shortTerm: (
       <List>
         <Item
           text={`Coordonner les différents services d'aide existants aux alentours pour les personnes en perte d'autonomie`}
@@ -200,8 +200,8 @@ export default [
           text={`Permettre au personnel administratif et technique de suivre des formations`}
         />
       </List>
-    </div>),
-    longTerm: (<div>
+    ),
+    longTerm: (
       <List>
         <Item
           text={`Projet de la création d'un espace de vie social avec un bureau de  permanence multifonction (assistante sociale, écoute jeunes) en partenariat avec la CAF`}
@@ -210,7 +210,7 @@ export default [
           text={`Obtention d'un agrément CAF pour assurer sa pérennisation`}
         />
       </List>
-    </div>)
+    )
   },
   {
     title: `Faire évoluer l'accueil des 3-18 ans`,
@@ -246,7 +246,7 @@ export default [
         />
       </List>
     </div>),
-    longTerm: (<div>
+    longTerm: (
       <List>
         <Item
           text={`Réflexion sur la création d'un nouveau bâtiment dédié au centre de loisirs plus proche de la nature`}
@@ -255,51 +255,51 @@ export default [
           text={`Réflexion sur la création d'un espace jeune`}
         />
       </List>
-    </div>)
+    )
   },
   {
     title: 'Diversifier les modes de garde',
     shortTerm: '',
-    longTerm: (<div>
+    longTerm: (
       <List>
         <Item
           text={`Etude de la création d'une micro-crèche`}
         />
       </List>
-    </div>)
+    )
   },
   {
     title: `Renforcer l'offre de santé`,
-    shortTerm: (<div>
+    shortTerm: (
       <List>
         <Item
           text={`Favoriser l'installation d'un médecin`}
         />
       </List>
-    </div>),
-    longTerm: (<div>
+    ),
+    longTerm: (
       <List>
         <Item
           text={`Préparer l'offre de santé à venir en lien avec les praticiens en exercice ( réflexion sur la création d'une maison de santé)`}
         />
       </List>
-    </div>) 
+    )
   },
   // Notre environnement
   {
     title: 'Installer le compostage collectif',
-    shortTerm: (<div>
+    shortTerm: (
       <List>
         <Item
           text={`Mettre en place une zone test  près des poubelles de la place du Gros Billot`}
         />
       </List>
-    </div>),  
+    ),
     longTerm: ''
   },
   {
     title: 'Encourager la mobilité piétonne',
-    shortTerm: (<div>
+    shortTerm: (
       <List>
         <Item
           text={`Installation de bancs sur l'ensemble du village`}
@@ -308,8 +308,8 @@ export default [
           text={`Abri bus aux Bruyères`}
         />
       </List>
-    </div>),
-    longTerm: (<div>
+    ),
+    longTerm: (
       <List>
         <Item
           text={`Réhabiliter la sente de Magonnet`}
@@ -321,51 +321,51 @@ export default [
           text={`Eclairage public muni de détecteur de mouvement (pollution nocturne et sécurité)`}
         />
       </List>
-    </div>)
+    )
   },
   {
     title: 'Faciliter les mobilités douces (vélos)',
-    shortTerm: (<div>
+    shortTerm: (
       <List>
         <Item
           text={`Mise en place d'espaces de stationnement `}
         />
       </List>
-    </div>),
-    longTerm: (<div>
+    ),
+    longTerm: (
       <List>
         <Item
           text={`Etude de la faisabilité de la liaison centre du village / pistes cyclables existantes et centre du village / quartier des Bruyères`}
         />
       </List>
-    </div>)
+    )
   },
   {
     title: `Arboriser la cour de l'école`,
-    shortTerm: (<div>
+    shortTerm: (
       <List>
         <Item
           text={`Plantation d'arbres dans la cour des maternelles (ombrage)`}
         />
       </List>
-    </div>),
+    ),
     longTerm: ''
   },
   {
     title: 'Mettre en plan le plan vigilance incendie',
-    shortTerm: (<div>
+    shortTerm: (
       <List>
         <Item
           text={`Réalisation et distribution d'un document résumant les diverses conduites à tenir en cas d'incendie`}
         />
       </List>
-    </div>),
+    ),
     longTerm: ''
   },
   {
     title: 'Lutter contre les nuisances sonores et de circulation',
     shortTerm: '',
-    longTerm: (<div>
+    longTerm: (
       <List>
         <Item
           text={`Etude de la mise en place de radars sonores (motos)`}
@@ -374,25 +374,25 @@ export default [
           text={`Etude de l'optimisation de la circulation au centre du village`}
         />
       </List>
-    </div>)
+    )
   },
   {
     title: 'Préserver le cadre urbain et le patrimoine',
-    shortTerm: (<div>
+    shortTerm: (
       <List>
         <Item
           text={`Végétalisation du cimetière`}
         />
       </List>
-    </div>),
+    ),
     longTerm: ''
   },
   // Les équilibres financiers
   {
-    title: 'gérer les coûts et maîtriser les finances',
+    title: 'Gérer les coûts et maîtriser les finances',
     shortTerm: '',
     longTerm: '',
-    learnMore: (<div>
+    learnMore: (
       <List>
         <Item
           text={`Maintenir le taux des taxes locales`}
@@ -401,11 +401,11 @@ export default [
           text={`Pas de démarrage de travaux avant la notification de subventions`}
         />
       </List>
-    </div>),
+    ),
   },
   {
     title: 'Renforcer la coopération de la commune avec les acteurs territoriaux',
-    subTitle: (<div>
+    subTitle: (
       <List>
         <Item
           text={`Rambouillet territoires (RT78)`}
@@ -426,10 +426,10 @@ export default [
           text={`Syndicat Intercommunal pour le Traitement et la Valorisation des déchets (SITREVA)`}
         />
       </List>
-    </div>),
+    ),
     shortTerm: '',
     longTerm: '',
-    learnMore: (<div>
+    learnMore: (
       <List>
         <Item
           text={`https://WWW.collectivites-locales.gouv.fr/finances-locales/obtenir-des-subventions`}
@@ -438,11 +438,11 @@ export default [
           text={`https://www.iledefrance.fr/aides-et-appels-a-projets/contrat-rural#:~:text=de%20coop%C3%A9ration%20intercommunale)-,Partager,-Partager%20sur%20Facebook`}
         />
       </List>
-    </div>)
+    )
   },
   {
     title: 'Rechercher des financements complémentaires',
-    subTitle: (<div>
+    subTitle: (
       <List>
         <Item
           text={`Agence de l'Environnement et de la Maîtrise de l'Energie (ADEME)`}
@@ -463,15 +463,15 @@ export default [
           text={`Parc Naturel Régional de la Haute Vallée de Chevreuse (PNR)`}
         />
       </List>
-    </div>),
+    ),
     shortTerm: '',
     longTerm: '',
-    learnMore: (<div>
+    learnMore: (
       <List>
         <Item
           text={`https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.parc-naturel-chevreuse.fr/le-fonctionnement/les=aides&ved=2ahUKEwjejYCx3ZaEAxUTT6QEHUaPBIIQFnoECBUQAQ&usg=AOvVaw1cDiuW3TVBR1KCldPr6ztz`}
         />
       </List>
-    </div>)
+    )
   },
 ];
