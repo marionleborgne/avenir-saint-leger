9acfdd5551b8cc9461f8e84e9e5b664bf3f01d83
Marion Le Borgne <leborgne.mar@gmail.com>
2024-02-08 22:26:33 -0800

    final cleanup

--- src/components/Projects/IntroStatement.js
+++ src/components/Projects/IntroStatement.js
@@ -1,7 +1,7 @@
 import { Typography } from '@mui/material';
 
 export default () => (
-  <div style={{ textAlign: 'center', margin: 10 }}>
+  <div style={{ textAlign: 'center', margin: '20px' }}>
     <Typography >
       {`Ce projet est ambitieux et réaliste. Notre village a un potentiel énorme. Avec vous et en toute transparence, nous pourrons lui offrir la qualité de vie et le dynamisme qu'il mérite.`}
     </Typography>
