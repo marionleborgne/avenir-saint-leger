28c95c649b1f0ae038523a0d84760df22fbb2df1
Marion Le Borgne <leborgne.mar@gmail.com>
2024-02-18 15:54:32 -0800

    Bouton insta

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import Button from '@mui/material/Button';
 import EmailIcon from '@mui/icons-material/EmailTwoTone';
-
+import InstagramIcon from '@mui/icons-material/Instagram';
 
 import Stripes from './Stripes/Stripes';
 import { SolidHighlight, HighlightContainer } from './Stripes/SolidHighlight';
@@ -10,7 +10,7 @@ export default () => (
   <HighlightContainer>
     <Stripes width='100%' height='200px' flipped />
     <SolidHighlight>
-      <div>
+      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }} >
         <Typography variant='h4' sx={{ margin: 5 }}>
           Nous contacter
         </Typography>
@@ -25,6 +25,18 @@ export default () => (
           style={{ textTransform: 'none', marginBottom: 20 }}
         >
           <Typography>contact@avenir-saint-leger.org</Typography>
+        </Button>
+        <Button
+          variant="text"
+          startIcon={<InstagramIcon />}
+          size="large"
+          color="secondary"
+          target="_top"
+          rel="noopener noreferrer"
+          href='https://www.instagram.com/avenir_saint_leger'
+          style={{ textTransform: 'none', color: '#000', marginBottom: 20 }}
+        >
+          <Typography>avenir_saint_leger</Typography>
         </Button>
       </div>
     </SolidHighlight>
